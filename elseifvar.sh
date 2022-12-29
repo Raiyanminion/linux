4a9eb3b1a0211480044c758c7ed0840a62d00ad5
Raiyanminion <112476106+Raiyanminion@users.noreply.github.com>
2022-12-29 16:11:53 +0530

    elseifvar.sh

---
+++ elseifvar.sh
@@ -0,0 +1,12 @@
+num_a=23
+num_b=100
+
+if [ $num_a -gt $num_b ];
+then 
+echo " $num_a is greater than $num_b "
+elif [ $num_a -lt $num_b ];
+then
+echo " $num_a is less than $num_b "
+else 
+echo " $num_a is equal to $num_b "
+fi
